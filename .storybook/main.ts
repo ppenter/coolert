62ac334f93fd8c929c22a53ef144f00d4d36f664
ppenter <jarupak.sri@gmail.com>
2024-02-08 02:29:27 +0700

    updated

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,6 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-info",
   ],
   framework: {
     name: "@storybook/react-vite",
