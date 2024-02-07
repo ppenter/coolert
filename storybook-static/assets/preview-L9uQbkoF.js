00c6c788883430c66c59b23855376099957598d7
ppenter <jarupak.sri@gmail.com>
2024-02-08 03:47:39 +0700

    add gh-pages

---
+++ storybook-static/assets/preview-L9uQbkoF.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./iframe-Jdz-iwlF.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-gcEt2GlJ.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{_ as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-gcEt2GlJ.js","./iframe-Jdz-iwlF.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./react-18-9dmwUyx5.js","./client-oSl7Hle7.js","./index-ogXoivrg.js","./extends-dGVwEr9R.js","./_getPrototype-bjD8Yebc.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
